3bf275558580e6059aef469f66b3748c6677447c
Diveesh Singh <diveesh@gmail.com>
2015-09-02 18:58:52 -0700

    refreshing database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818054204) do
+ActiveRecord::Schema.define(version: 20150903012438) do
 
   create_table "activities", force: true do |t|
     t.string   "name"
